6c9611a3c10908a94cbeb4d7aad0b19dde7207ba
VinzR-2159109 <vinz.roosen@student.uhasselt.be>
2023-11-23 19:55:39 +0100

    Add pages of 20 products

--- productbookwidget.h
+++ productbookwidget.h
@@ -27,6 +27,8 @@ private:
 
     ProductTrie m_productTrie;
     Repository m_repository;
+    QLabel *m_pageLabel;
+    int m_pageNumber;
 
     /**
      * @brief Loads the data from the file
@@ -38,7 +40,9 @@ private:
      * @param item - the item to display
      */
     void displaySelectedProduct(QListWidgetItem *item);
-
+    void nextPage();
+    void prevPage();
+    void calculatePageCounters(int &begin, int &end);
 private slots:
 
     /**
