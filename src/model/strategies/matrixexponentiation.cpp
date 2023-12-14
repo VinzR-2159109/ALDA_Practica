1687e97017c4cb14a70eeebc4f20e898f610be74
LarsG-2158052 <lars.gielen@student.uhasselt.be>
2023-12-14 23:41:30 +0100

    Matrix Solution

--- src/model/strategies/matrixexponentiation.cpp
+++ src/model/strategies/matrixexponentiation.cpp
@@ -1,64 +1,195 @@
 #include "matrixexponentiation.h"
 #include <iostream>
 
+/**
+ * Deze strategie werkt als volgt:
+ *
+ *  1. Maak een graph aan door alle connecties in een adjacency matrix te steken.
+ *  2. Maak een 'reachable' matrix waarbij
+ *      - 1 betekend dat vertex B bereikbaar is vanuit vertex A binnen het aantal dagen.
+ *      - 0 betekend dat vertex B niet bereikbaar is vanuit vertex A binnen het aantal dagen.
+ *  3. Bereken de decimale waarde van de juiste oplossing
+ *      - Bijvoorbeeld: Vertex A = besmet, B = niet besmet, C = besmet -> 101 -> 5
+ *  4. Bereken de decimale waarde van alle rijen van de 'reachable' matrix
+ *      - Bijvoorbeeld:   A B C
+ *                      A 1 0 0 :: -> 4
+ *                      B 0 1 0 :: -> 2
+ *                      C 0 0 1 :: -> 1
+ *  5. Vind het kleinste aantal waardes dat samen de waarde van de juiste oplossing is
+ *      - 5 = 4 + 1 -> A en C zijn de oplossing
+ *
+ *  Nadelen van deze methode:
+ *      1. /
+ *
+ *  Voordelen van deze methode:
+ *      1. Onafhankelijk van het aantal edges
+ */
 QVector<Vertex *> MatrixExponentiation::execute()
-{
-    createMatrix();
-    return {};
-}
-
-// Sources:
-// https://www.geeksforgeeks.org/matrix-exponentiation/
-// https://cp-algorithms.com/algebra/binary-exp.html
-// https://cs.stackexchange.com/questions/1147/complexity-of-computing-matrix-powers
-void MatrixExponentiation::createMatrix()
 {
     int matrixSize = m_data->getVertices().size();
 
-    // init matrix
-    m_matrix = QVector<QVector<int>>(matrixSize);
+    QVector<QVector<bool>> adjacencyMatrix = QVector<QVector<bool>>(matrixSize);
+    QVector<QVector<bool>> reachableMatrix;
+
+    // Init adjacencyMatrix
     for (int i = 0; i < matrixSize; ++i) {
-        m_matrix[i] = QVector<int>(matrixSize);
+        adjacencyMatrix[i] = QVector<bool>(matrixSize);
     }
 
-    // add connections
-    QHash<Vertex*, int> vertexIndexHash;
-    QHash<int, Vertex*> indexVertexHash;
+    createAdjacencyMatrix(adjacencyMatrix);
+    createReachableMatrix(adjacencyMatrix, reachableMatrix, m_data->getDays());
 
+    // FOR TESTING --------------------
+    printMatrix(reachableMatrix);
+
+    std::cout << "Target value: ";
+    int targetValue = 0;
+    for (const auto &vertex : m_data->getVertices()) {
+        int index = m_vertexIndexHash.value(vertex);
+        targetValue += std::pow(2, index) * vertex->getIsInfected();
+        std::cout << vertex->getIsInfected() << " ";
+    }
+    std::cout << ":: " << targetValue << "\n";
+    // --------------------------------
+
+    return findSources(reachableMatrix);
+}
+
+void MatrixExponentiation::createAdjacencyMatrix(QVector<QVector<bool>> &adjacencyMatrix)
+{
     int previousIndex = 0;
-    for (auto it = m_data->getConnections().cbegin(); it != m_data->getConnections().cend(); it++) {
+    for (const auto &vertex : m_data->getVertices()) {
+        m_vertexIndexHash.insert(vertex, previousIndex);
+        m_indexVertexHash.insert(previousIndex, vertex);
 
-        // if new vertex -> place in index hash
-        if (!vertexIndexHash.contains(it.key())) {
-            vertexIndexHash.insert(it.key(), previousIndex);
-            indexVertexHash.insert(previousIndex, it.key());
-            previousIndex++;
-        }
+        adjacencyMatrix[previousIndex][previousIndex] = 1;
 
-        // if new vertex -> place in index hash
-        if (!vertexIndexHash.contains(it.value())) {
-            vertexIndexHash.insert(it.value(), previousIndex);
-            indexVertexHash.insert(previousIndex, it.value());
-            previousIndex++;
-        }
-
-        int indexFrom = vertexIndexHash.value(it.key());
-        int indexTo = vertexIndexHash.value(it.value());
-
-        m_matrix[indexFrom][indexTo] = 1;
+        previousIndex++;
     }
 
-    // Print the adjacency matrix
+    for (auto it = m_data->getConnections().cbegin(); it != m_data->getConnections().cend(); it++) {
+        int indexFrom = m_vertexIndexHash.value(it.key());
+        int indexTo = m_vertexIndexHash.value(it.value());
+
+        adjacencyMatrix[indexFrom][indexTo] = 1;
+    }
+}
+
+void MatrixExponentiation::createReachableMatrix(QVector<QVector<bool>> &adjacencyMatrix, QVector<QVector<bool>> &reachableMatrix, int days)
+{
+    reachableMatrix = adjacencyMatrix;
+
+    QVector<QVector<bool>> m1 = adjacencyMatrix;
+    QVector<QVector<bool>> m2 = adjacencyMatrix;
+
+    QVector<QVector<bool>> *previousResult = &m1;
+    QVector<QVector<bool>> *temp = &m2;
+
+    int matrixSize = adjacencyMatrix.size();
+
+    // Bereken welke vertices elkaar kunnen bereiken binnen het aantal dagen
+    for (int day = 0; day < days - 1; day++) {
+
+        // Matrix multiplicatie
+        for (int i = 0; i < matrixSize; i++) {
+            for (int j = 0; j < matrixSize; j++) {
+                (*temp)[i][j] = 0;
+                for (int k = 0; k < matrixSize; k++) {
+                    if ((*previousResult)[i][k] * adjacencyMatrix[k][j] == 1) {
+                        (*temp)[i][j] = 1;
+                    }
+                }
+            }
+        }
+
+        // Set temp in previousResult (switch pointers)
+        QVector<QVector<bool>> *pointer = previousResult;
+        previousResult = temp;
+        temp = pointer;
+
+        // Update reachableMatrix
+        for (int i = 0; i < matrixSize; i++) {
+            for (int j = 0; j < matrixSize; j++) {
+                reachableMatrix[i][j] += (*previousResult)[i][j];
+            }
+        }
+    }
+}
+
+QVector<Vertex*> MatrixExponentiation::findSources(QVector<QVector<bool>> &reachableMatrix)
+{
+    // Calculate decimal target value
+    int targetValue = 0;
+    for (const auto &vertex : m_data->getVertices()) {
+        int index = m_vertexIndexHash.value(vertex);
+        targetValue += std::pow(2, index) * vertex->getIsInfected();
+    }
+
+    // Calculate decimal value of each row
+    QVector<int> rowValues;
+    for (const auto &row : reachableMatrix) {
+        int value = 0;
+        for (int i = 0; i < row.size(); ++i) {
+            value += std::pow(2, i) * row[i];
+        }
+        rowValues.push_back(value);
+    }
+
+    // Find smallest amount of values that add up to targetValue -> O(oneindig)
+
+    // TODO: Voor nu ga ik er vanuit dat er max 1 oplossing is
+    for (int i = 0; i < rowValues.size(); i++) {
+        if (rowValues[i] == targetValue)
+            return {m_indexVertexHash.value(i)};
+    }
+
+    return {};
+}
+
+// FOR TESTING
+void MatrixExponentiation::printMatrix(QVector<QVector<bool>> &matrix)
+{
+    int matrixSize = matrix.size();
+
     std::cout << "   ";
     for (int j = 0; j < matrixSize; ++j) {
-        std::cout << indexVertexHash.value(j)->getName().toStdString() << " ";
+        std::cout << m_indexVertexHash.value(j)->getName().toStdString() << " ";
     }
     std::cout << "\n";
     for (int i = 0; i < matrixSize; ++i) {
-        std::cout << indexVertexHash.value(i)->getName().toStdString() << ": ";
+        std::cout << m_indexVertexHash.value(i)->getName().toStdString() << ": ";
         for (int j = 0; j < matrixSize; ++j) {
-            std::cout << m_matrix[i][j] << " ";
+            std::cout << matrix[i][j] << " ";
         }
-        std::cout << std::endl;
+
+        // calculate decimal value
+        int value = 0;
+        for (int j = 0; j < matrixSize; ++j) {
+            value += std::pow(2, j) * matrix[i][j];
+        }
+
+        std::cout << ":: " << value << "\n";
     }
+    std::cout << "\n";
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
