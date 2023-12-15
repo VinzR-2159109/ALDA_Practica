479afbf42bf8f22b21b35777c2f062fff0b1b160
LarsG-2158052 <lars.gielen@student.uhasselt.be>
2023-12-15 17:24:24 +0100

    Add Tests

--- Test/tst_reachablematrix.cpp
+++ Test/tst_reachablematrix.cpp
@@ -127,6 +127,34 @@ TEST(Test_Reachable_Matrix, Infected_Vertex_Out_Of_Range) {
     }
 }
 
+TEST(Test_Reachable_Matrix, Multiple_sources_In_Same_Graph) {
+    // Create strategy
+    BaseStrategy *currentStrategy = StrategyFactory().getStrategy(StrategyFactory::AllStrategies::ReachableMatrix);
+    StrategyContext context;
+
+    // Give data to strategy
+    context.setStrategy(currentStrategy);
+
+    // Load data
+    QString filePath = QDir::currentPath().append("/data/2_sources_in_same_graph.txt");
+    GraphData *data = Repository().LoadFile(filePath).graphData;
+    context.getStrategy()->setData(data);
+
+    // Run strategy
+    auto result = context.runStrategy();
+
+    // Check if result is correct
+    if (result.size() != 2)
+        FAIL() << "Result does not have the correct amount of Vertices";
+
+    for (const auto &resultVertex : result) {
+        if (!QString("AD").contains(resultVertex->getName())) {
+            FAIL() << "Result is not the correct solution";
+            break;
+        }
+    }
+}
+
 TEST(Test_Reachable_Matrix, No_Connections) {
     // Create strategy
     BaseStrategy *currentStrategy = StrategyFactory().getStrategy(StrategyFactory::AllStrategies::ReachableMatrix);
